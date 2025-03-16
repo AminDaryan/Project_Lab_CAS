ee64d819050b77095a8e5e578e1ddb14d5f9e1d5
AMIN DARIAN <amindarian@gmail.com>
2025-03-16 14:25:02 +0100

    wip: add new trained model

--- train_model.py
+++ train_model.py
@@ -10,11 +10,24 @@ if __name__ == "__main__":
     torch.multiprocessing.set_start_method('spawn', force=True)  # Ensure proper multiprocessing handling
 
     # Load a model
-    model = YOLO("yolov8n.pt")  # Load pre-trained model
+    model = YOLO("yolo11n.pt")  # load pre trained model
 
     # Use the model
-    results = model.train(
-        data=os.path.abspath(os.path.join("datasets/Target_Cube_Object_Detection_2", "data.yaml")),
-        epochs=20,
-        workers=0  # Disable multiprocessing to avoid Windows issues
+    
+    # Train the model
+    train_results = model.train(
+        data=os.path.abspath(os.path.join("datasets/Target_Cube_Object_Detection_2", "data.yaml")),  # path to dataset YAML
+        epochs=100,  # number of training epochs
+        imgsz=640,  # training image size
+        device="cpu",  # device to run on, i.e. device=0 or device=0,1,2,3 or device=cpu
     )
+    
+    # Evaluate model performance on the validation set
+    metrics = model.val()
+
+    # Perform object detection on an image
+    results = model("path/to/image.jpg")
+    results[0].show()
+
+    # Export the model to ONNX format
+    path = model.export(format="onnx")  # return path to exported model
